1d83329aeb6802333039a79c29e25e461b2149ca
Faith Morante <faith.morante@gmail.com>
2019-02-18 18:47:40 +0800

    test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const IndexPage = () => (
                 </Col>
             </Row>
             <Row style={{height: '50%'}}>
-                <Col>
+                <Col>test
                     <img src={honey3}/>
                 </Col>
                 <Col>
