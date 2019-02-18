64295766109dd3384679b86a925aeaf74cbf7634
Faith <faith.morante@honcho.com>
2019-02-18 12:55:39 +0800

    boxes design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,50 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import './../fonts/fonts.css'
+import './../css/style.css'
+import honey1 from './../images/honey1.jpeg'
+import honey2 from './../images/honey2.jpg'
+import honey3 from './../images/honey3.jpg'
+import honey4 from './../images/honey4.jpg'
+import soundcloud from './../images/soundcloud.png'
+import youtube from './../images/youtube.jpeg'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="full-page">
+        <Container>
+            <Row style={{height: '50%'}}>
+                <Col className="main-title">
+                    Honey Morante
+                </Col>
+                <Col>
+                    <img src={honey1} />
+                </Col>
+                <Col><a href="https://www.youtube.com/user/honzM11"><img src={youtube} /></a>
+                </Col>
+            </Row>
+            <Row style={{height: '50%'}}>
+                <Col>
+                    <img src={honey3}/>
+                </Col>
+                <Col>
+                    <Row style={{height: '50%'}}>
+                        <a href="https://soundcloud.com/honeymorante"><img src={soundcloud} /></a>
+                    </Row>
+                    <Row style={{height: '50%'}}>
+                        <img src={honey2} />
+                    </Row>
+                </Col>
+                <Col>
+                    <Row style={{height: '50%'}}>
+                        <img src={honey4} />
+                    </Row>
+                    <Row style={{height: '50%', textAlign: 'center'}}>
+                        <span style={{margin: '0 auto'}}>Blog - coming soon</span>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
-
+);
 export default IndexPage
