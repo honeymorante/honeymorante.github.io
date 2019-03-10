278681b740ce27cc7e0f318e89a363f1ff6b73fb
Faith Morante <faith.morante@gmail.com>
2019-03-10 20:45:22 +0800

    updated pics

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,20 @@ import honey1 from './../images/honey1.jpg'
 import honey2 from './../images/honey2.jpg'
 import honey3 from './../images/honey3.jpg'
 import honey4 from './../images/honey4.jpg'
+import honeynew1 from './../images/honey_new1.jpg'
+import honeynew2 from './../images/honey_new2.jpg'
+import honeynew3 from './../images/honey_new3.jpg'
+import honeynew4 from './../images/honey_new4.jpg'
 import soundcloud from './../images/soundcloud.jpg'
 import youtube from './../images/youtube.jpg'
+import { Helmet } from 'react-helmet';
 
 const IndexPage = () => (
     <div className="full-page">
+        <Helmet>
+            <title>Honey Morante</title>
+            <meta name="description" content="Honey Morante - Thespian | Singer | Songwriter | Teacher | Ukulele Player" />
+        </Helmet>
         <Container>
             <Row style={{height: '50%'}}>
                 <Col className="main-box">
@@ -20,7 +29,7 @@ const IndexPage = () => (
                     <span className="main-sub-title">Thespian | Singer | Songwriter | Teacher | Ukulele Player</span>
                 </Col>
                 <Col className="img-parent" style={{height: '100%'}}>
-                    <img src={honey1} />
+                    <img src={honeynew1} />
                 </Col>
                 <Col className="img-parent">
                     <a href="https://www.youtube.com/user/honzM11" style={{height: '100%'}}>
@@ -30,7 +39,7 @@ const IndexPage = () => (
             </Row>
             <Row style={{height: '50%'}}>
                 <Col className="img-parent">
-                    <img src={honey3}/>
+                    <img src={honeynew2}/>
                 </Col>
                 <Col style={{height: '100%'}}>
                     <Row style={{height: '50%'}} className="img-parent">
@@ -38,12 +47,12 @@ const IndexPage = () => (
                         <img src={soundcloud}  /></a>
                     </Row>
                     <Row style={{height: '50%'}} className="img-parent">
-                        <img src={honey2} />
+                        <img src={honeynew3} />
                     </Row>
                 </Col>
                 <Col style={{height: '100%'}}>
                     <Row style={{height: '50%'}} className="img-parent">
-                        <img src={honey4} />
+                        <img src={honeynew4} />
                     </Row>
                     <Row style={{height: '50%', textAlign: 'center', backgroundColor: '#e8c7cc'}}>
                         <span style={{margin: '0 auto'}}>Blog - coming soon</span>
